1127d341b850c28c9f1945a22bd4b72af64ea49d
shaheenkaipally <shaheenkm3@outlook.com>
2021-02-27 10:46:03 +0530

    a

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /*  home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Homes' });
+    res.render('index', { title: 'Home' });
 });
 router.get('/home', function(req, res, next) {
     res.render('index', { title: 'Home' });
