8086915ff0c021cc5497b166173e9d6a98b0276c
shaheenkaipally <shaheenkm3@outlook.com>
2021-02-27 10:41:51 +0530

    new pages

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+
 /*  home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Home' });
+    res.render('index', { title: 'Homes' });
 });
 router.get('/home', function(req, res, next) {
     res.render('index', { title: 'Home' });
 });
 /*  about me page. */
 router.get('/about', function(req, res, next) {
-    res.render('index', { title: 'About' });
+    res.render('about', { title: 'About' });
 });
 /*  project page. */
 router.get('/project', function(req, res, next) {
-    res.render('index', { title: 'Project' });
+    res.render('about', { title: 'Project' });
 });
 /*  contact page. */
 router.get('/contact', function(req, res, next) {
-    res.render('index', { title: 'Contact' });
+    res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
